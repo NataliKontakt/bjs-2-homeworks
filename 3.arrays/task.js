364364651ce1d9c5ce655e1d0fe8113d696ae553
“Natali <natali-kontakt@yandex.ru>
2023-02-12 23:35:07 +0500

    add task Arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,16 @@
+
 function compareArrays(arr1, arr2) {
-  
+    return (arr1.length === arr2.length && 
+            arr1.every((n, i) => n === arr2[i]));
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
-}
+    let result = users.filter(users => users.gender === gender).map(users => users.age).reduce((acc, item, index, users) => {
+      acc += item, 0;
+      if (index === users.length - 1) {
+        return acc / users.length;
+      }
+      return acc;
+    }, 0)
+    return result
+  }
